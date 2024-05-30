21529bef35a6b81312f9eecd87407203943c2ce8
Josue Maldonado <memejosue2012@hotmail.com>
2024-05-30 11:12:49 -0600

    Arreglo error app.py, puerto 5000

--- mi_proyecto_flask/app.py
+++ mi_proyecto_flask/app.py
@@ -14,4 +14,4 @@ def home():
                            facultad=facultad,curso=curso, edad=edad)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
