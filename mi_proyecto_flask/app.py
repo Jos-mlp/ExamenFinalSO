7405f66980b81dc77fe6f1d62dc9899483118d37
Josue Maldonado <memejosue2012@hotmail.com>
2024-05-30 10:58:50 -0600

    Aplicacion en flasj

---
+++ mi_proyecto_flask/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    nombre = "Josue Manuel Maldonado Lepe"
+    carnet = "1664220"
+    url="Universidad Rafael Landivar"
+    facultad="Facultad ingenieria"
+    curso="Sistemas operativos"
+    edad = 21  
+    return render_template('index.html', nombre=nombre,carnet=carnet,url=url,
+                           facultad=facultad,curso=curso, edad=edad)
+
+if __name__ == '__main__':
+    app.run(debug=True)
